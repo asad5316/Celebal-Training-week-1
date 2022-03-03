82f8cc28ab0858478b1eed5fb3d0a930b71dd668
Asad shaikh <64421307+asad5316@users.noreply.github.com>
2022-03-03 11:52:02 +0530

    Add files via upload

---
+++ print-function.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+          
+    """for i in range(1,n+1):
+        print(i,end="")"""
+    i=1
+    while i<=n:
+        print(i,end='')
+        i=i+1
